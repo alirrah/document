cf1b8b26ebb71268bdc2e6621817c605528c7474
alirrah <alir.rah81@gmail.com>
2024-09-15 22:38:39 +0330

    [ADD] protected route

---
+++ src/theme/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "@theme-original/Layout";
+import type LayoutType from "@theme/Layout";
+import type { WrapperProps } from "@docusaurus/types";
+import ProtectedRoute from "../../components/protected-route";
+
+type Props = WrapperProps<typeof LayoutType>;
+
+export default function LayoutWrapper(props: Props): JSX.Element {
+  return (
+    <ProtectedRoute>
+      <Layout {...props} />
+    </ProtectedRoute>
+  );
+}
