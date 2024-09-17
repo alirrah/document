5bdb7c84c9243a982f48d91858429a4ab525c5bc
alirrah <alir.rah81@gmail.com>
2024-09-17 10:47:44 +0330

    [ADD] Error component

---
+++ src/components/error.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import styles from "./error.module.css";
+
+interface TypingEffectProps {
+  text: string;
+  speed: number;
+  getData: () => void;
+}
+
+const Error: React.FC<TypingEffectProps> = ({ text, speed, getData }) => {
+  const [displayedText, setDisplayedText] = useState<string>("");
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setDisplayedText((prev) => prev + text.charAt(index++));
+
+      if (index === text.length) {
+        clearInterval(interval);
+      }
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return (
+    <>
+      <p>{displayedText}</p>
+      <button onClick={getData} className={styles.button}>
+        <p>درخواست مجدد</p>
+      </button>
+    </>
+  );
+};
+
+export default Error;
--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "@theme-original/Layout";
 import type LayoutType from "@theme/Layout";
 import type { WrapperProps } from "@docusaurus/types";
-import ProtectedRoute from "../../components/protected-route";
+import ProtectedRoute from "./../../components/protected-route";
 
 type Props = WrapperProps<typeof LayoutType>;
 
