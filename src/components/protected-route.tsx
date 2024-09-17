23bb6f49e76dc3be1815d05419003018658ca675
alirrah <alir.rah81@gmail.com>
2024-09-17 12:56:27 +0330

    [EDIT] check jwt format saved in local storage

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -10,8 +10,21 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   children,
   ...rest
 }) => {
-  const token: string | null = localStorage.getItem("jwt");
-  const isAuthenticated: boolean = !!token;
+  const isJsonFormat = (text: string) => {
+    try {
+      const tokens = JSON.parse(text);
+
+      return (
+        tokens.hasOwnProperty("access") && tokens.hasOwnProperty("refresh")
+      );
+    } catch (error) {
+      localStorage.removeItem("jwt");
+      return false;
+    }
+  };
+
+  const tokens: string | null = localStorage.getItem("jwt");
+  const isAuthenticated: boolean = !!tokens && isJsonFormat(tokens);
 
   return (
     <Route
