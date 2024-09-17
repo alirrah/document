0b989f822d02a8e259c2052e81b0ac7d17d92160
alirrah <alir.rah81@gmail.com>
2024-09-17 12:55:33 +0330

    [ADD] show content

---
+++ docs/intro.md
@@ -0,0 +1,11 @@
+---
+id: my-document
+title: عنوان سند
+description: توضیحات سند
+slug: /my-custom-path
+sidebar_position: 1
+---
+
+import ApiMarkdownContent from '@site/src/components/api-markdown-content';
+
+<ApiMarkdownContent id={1} />
---
+++ src/components/api-markdown-content.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import Loading from "./loading";
+import Error from "./error";
+import config from "./../../docusaurus.config";
+
+interface ApiMarkdownContentProps {
+  id: number;
+}
+
+const ApiMarkdownContent: React.FC<ApiMarkdownContentProps> = ({ id }) => {
+  const history = useHistory();
+  const url = "http://127.0.0.1:8000";
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    setContent("");
+
+    const token: { access: string; refresh: string } = JSON.parse(
+      localStorage.getItem("jwt")
+    );
+    try {
+      const response = await axios.get(`${url}/documents/${id}/`, {
+        headers: {
+          Authorization: `Bearer ${token.access}`,
+        },
+      });
+      setContent(response.data.content);
+    } catch (err: any) {
+      if (err.response) {
+        if (err.response.status === 401) {
+          try {
+            const response = await axios.post(`${url}/jwt/token/refresh/`, {
+              refresh: token.refresh,
+            });
+            localStorage.setItem("jwt", JSON.stringify(response.data));
+
+            return fetchData();
+          } catch (error) {
+            localStorage.removeItem("jwt");
+            history.replace(`${config.baseUrl}login`);
+          }
+        } else {
+          setError(
+            "خطایی در ارسال درخواست به موجود آماده است. شماره خطا: " +
+              err.response.status
+          );
+        }
+      } else {
+        setError("خطایی در ارسال درخواست به موجود آماده است.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [id]);
+
+  return (
+    <>
+      {error ? (
+        <Error text={error} speed={50} getData={fetchData} />
+      ) : loading ? (
+        <Loading />
+      ) : (
+        <ReactMarkdown>{content}</ReactMarkdown>
+      )}
+    </>
+  );
+};
+
+export default ApiMarkdownContent;
