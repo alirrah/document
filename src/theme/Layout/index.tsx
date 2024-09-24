40ad31cb81c879527c6903541d1677ca9d2a9864
alirrah <alir.rah81@gmail.com>
2024-09-24 17:51:24 +0330

    [EDIT] make layout component client side

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import Layout from "@theme-original/Layout";
 import type LayoutType from "@theme/Layout";
 import type { WrapperProps } from "@docusaurus/types";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import ProtectedRoute from "@site/src/components/ProtectedRoute";
 
 type Props = WrapperProps<typeof LayoutType>;
 
 export default function LayoutWrapper(props: Props): JSX.Element {
   return (
-    <ProtectedRoute>
-      <Layout {...props} />
-    </ProtectedRoute>
+    <BrowserOnly>
+      {() => (
+        <ProtectedRoute>
+          <Layout {...props} />
+        </ProtectedRoute>
+      )}
+    </BrowserOnly>
   );
 }
